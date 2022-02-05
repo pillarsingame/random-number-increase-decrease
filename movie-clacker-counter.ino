c40b3705f9734a0e2d8c77f2ce881c56ceac6395
thefirethirteen <thefirethirteen@protonmail.com>
2022-02-05 11:49:43 +0200

    Add support for the increment counter button

--- movie-clacker-counter.ino
+++ movie-clacker-counter.ino
@@ -1,14 +1,27 @@
 #include "display.h"
 
+const int buttonPin = 13;
+
+int buttonState = 0;
+
 // the setup routine runs once when you press reset:
 void setup() {                
   setupDisplay();
 }
 
 // test counter
-int counter = 76;
+int counter = 0;
 
 // the loop routine runs over and over again forever:
 void loop() {
   writeNumber(counter);
+
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH)
+  {
+    counter++;
+
+    delay(200);
+  }
 }
